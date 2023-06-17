487111a776eedabaa1c1666fd69a7107cdc4956d
dinesh11515 <dineshaitham2@gmail.com>
2023-06-17 21:46:33 +0530

    added wallet connection in dashboard

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,47 +1,70 @@
-import Job from '@/components/Job/Job';
-import React, { useState } from 'react';
-import { BsArrowDownShort } from 'react-icons/bs';
-
+import Job from "@/components/Job/Job";
+import React, { useState } from "react";
+import { BsArrowDownShort } from "react-icons/bs";
+import { useAccount } from "wagmi";
+import LIT from "@/components/LIT/LIT";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 const Dashboard = () => {
   const [jobData, setJobData] = useState([]);
+  const [pkpWallet, setPkpWallet] = useState(null);
+
+  const { address } = useAccount();
+
+  const getData = async () => {
+    try {
+      const walletAddress = address ? address : pkpWallet.address;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div className=' min-h-[95vh] bg-[#1C1C1F]'>
-      <div className='w-[95%] mx-auto py-10'>
-        <h2 className='text-xl flex items-center gap-3 font-semibold text-gray-400 font-Poppins '>
-          My Automated Jobs <BsArrowDownShort size={25} />
-        </h2>
+    <div className=" min-h-[95vh] bg-[#1C1C1F]">
+      <div className="w-[95%] mx-auto py-10">
+        {!(address || pkpWallet) ? (
+          <div className="flex flex-col items-center gap-2">
+            <LIT setPkpWallet={setPkpWallet} />
+            <p className="text-center text-gray-300">or</p>
+            <ConnectButton />
+          </div>
+        ) : (
+          <div>
+            <h2 className="text-xl flex items-center gap-3 font-semibold text-gray-400 font-Poppins ">
+              My Automated Jobs <BsArrowDownShort size={25} />
+            </h2>
 
-        <div className='flex flex-col gap-5 mt-10 '>
-          {jobData.map((job) => (
-            <Job
-              automationType='timebased'
-              contractAddress='0x433F4d3ED23f169E465C06AB73c8e025f4e4f8Be'
-              functionName='transfer'
-              isExecuted={false}
-              scheduledTime={1687356180}
-              jobName='Test Job'
-              schedulerAddress='0x51EEBc7765b246a4D16d02b28CEAC61299AB7d9d'
-              registeredDate={1687000586}
-              noOfTimesExectued={0}
-              listOfTimesFuncExectued={[]}
-            />
-          ))}
+            <div className="flex flex-col gap-5 mt-10 ">
+              {jobData.map((job) => (
+                <Job
+                  automationType="timebased"
+                  contractAddress="0x433F4d3ED23f169E465C06AB73c8e025f4e4f8Be"
+                  functionName="transfer"
+                  isExecuted={false}
+                  scheduledTime={1687356180}
+                  jobName="Test Job"
+                  schedulerAddress="0x51EEBc7765b246a4D16d02b28CEAC61299AB7d9d"
+                  registeredDate={1687000586}
+                  noOfTimesExectued={0}
+                  listOfTimesFuncExectued={[]}
+                />
+              ))}
 
-          {/* Isko delete kr dena */}
-          <Job
-            automationType='customLogic'
-            contractAddress='0x433F4d3ED23f169E465C06AB73c8e025f4e4f8Be'
-            functionName='Proxy Call'
-            isExecuted={true}
-            scheduledTime={1687356180}
-            jobName='Prxoy Job 3'
-            schedulerAddress='0x51EEBc7765b246a4D16d02b28CEAC61299AB7d9d'
-            registeredDate={1687000586}
-            noOfTimesExectued={4}
-            listOfTimesFuncExectued={[1687000586, 1687356180]}
-          />
-        </div>
+              {/* Isko delete kr dena */}
+              <Job
+                automationType="customLogic"
+                contractAddress="0x433F4d3ED23f169E465C06AB73c8e025f4e4f8Be"
+                functionName="Proxy Call"
+                isExecuted={true}
+                scheduledTime={1687356180}
+                jobName="Prxoy Job 3"
+                schedulerAddress="0x51EEBc7765b246a4D16d02b28CEAC61299AB7d9d"
+                registeredDate={1687000586}
+                noOfTimesExectued={4}
+                listOfTimesFuncExectued={[1687000586, 1687356180]}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -1,7 +1,7 @@
-import { contractABI, contractAddress } from '@/constants';
-import { waitForTransaction } from '@wagmi/core';
-import { ethers } from 'ethers';
-import { writeContract } from '@wagmi/core';
+import { contractABI, contractAddress } from "@/constants";
+import { waitForTransaction } from "@wagmi/core";
+import { ethers } from "ethers";
+import { writeContract } from "@wagmi/core";
 
 export const getContract = async (signer) => {
   return new ethers.Contract(contractAddress, contractABI, signer);
@@ -15,7 +15,7 @@ export const registerKeeper = async (address, amount, pkpWallet) => {
       const { hash } = await writeContract({
         address: contractAddress,
         abi: contractABI,
-        functionName: 'registerKeeper',
+        functionName: "registerKeeper",
         args: [address, amountInWei],
         value: amountInWei,
       });
@@ -31,21 +31,106 @@ export const registerKeeper = async (address, amount, pkpWallet) => {
 const pkpRegisterKeeper = async (signer, address, amount) => {
   try {
     const provider = new ethers.providers.JsonRpcProvider(
-      'https://rpc.ankr.com/filecoin_testnet'
+      "https://rpc.ankr.com/filecoin_testnet"
     );
-
     const amountInWei = ethers.utils.parseEther(amount);
-
     const contract = await getContract(provider);
     const unsignedTransaction =
       await contract.populateTransaction.registerKeeper(address, amountInWei);
     const data = unsignedTransaction.data;
+    await sendPkpTransaction(data, provider, signer, amountInWei);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+export const deposit = async (address, amount, pkpWallet) => {
+  try {
+    if (pkpWallet) {
+      await pkpDeposit(pkpWallet, address, amount);
+    } else {
+      const amountInWei = ethers.utils.parseEther(amount);
+      const { hash } = await writeContract({
+        address: contractAddress,
+        abi: contractABI,
+        functionName: "deposit",
+        args: [address],
+        value: amountInWei,
+      });
+      await waitForTransaction({
+        hash: hash,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const pkpDeposit = async (signer, address, amount) => {
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://rpc.ankr.com/filecoin_testnet"
+    );
+
+    const amountInWei = ethers.utils.parseEther(amount);
+    const contract = await getContract(provider);
+    const unsignedTransaction = await contract.populateTransaction.deposit(
+      address
+    );
+    const data = unsignedTransaction.data;
+    await sendPkpTransaction(data, provider, signer, amountInWei);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const withdraw = async (address, amount, pkpWallet) => {
+  try {
+    if (pkpWallet) {
+      await pkpWithdraw(pkpWallet, address, amount);
+    } else {
+      const amountInWei = ethers.utils.parseEther(amount);
+      const { hash } = await writeContract({
+        address: contractAddress,
+        abi: contractABI,
+        functionName: "deposit",
+        args: [address, amountInWei],
+        value: 0,
+      });
+      await waitForTransaction({
+        hash: hash,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const pkpWithdraw = async (signer, address, amount) => {
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://rpc.ankr.com/filecoin_testnet"
+    );
+    const amountInWei = ethers.utils.parseEther(amount);
+    const contract = await getContract(provider);
+    const unsignedTransaction = await contract.populateTransaction.withdraw(
+      address,
+      amountInWei
+    );
+    const data = unsignedTransaction.data;
+    await sendPkpTransaction(data, provider, signer, amountInWei);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const sendPkpTransaction = async (data, provider, signer, amountInWei) => {
+  try {
     let nonce = await provider.getTransactionCount(signer.address);
-    console.log('Nonce:', nonce);
+    console.log("Nonce:", nonce);
 
     let feeData = await provider.getFeeData();
-    console.log('Fee Data:', feeData);
+    console.log("Fee Data:", feeData);
 
     const tx = {
       type: 2,
@@ -60,13 +145,13 @@ const pkpRegisterKeeper = async (signer, address, amount) => {
       chainId: 314159,
       data: data,
     };
-    console.log('Transaction Data:', tx);
+    console.log("Transaction Data:", tx);
 
     const signedTx = await signer.signTransaction(tx);
-    console.log('Signed Transaction:', signedTx);
+    console.log("Signed Transaction:", signedTx);
 
     const txHash = ethers.utils.keccak256(signedTx);
-    console.log('Precomputed txHash:', txHash);
+    console.log("Precomputed txHash:", txHash);
 
     provider.sendTransaction(signedTx).then(async (tx) => {
       await tx.wait();
@@ -76,43 +161,19 @@ const pkpRegisterKeeper = async (signer, address, amount) => {
   }
 };
 
-// export const deposit = async (signer, address, amount) => {
-//   try {
-//     const contract = await getContract(signer);
-//     const amountInWei = ethers.utils.parseEther(amount);
-//     const tx = await contract.deposit(address, {
-//       value: amountInWei,
-//     });
-//     await tx.wait();
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// export const withdraw = async (signer, address, amount) => {
-//   try {
-//     const contract = await getContract(signer);
-//     const amountInWei = ethers.utils.parseEther(amount);
-//     const tx = await contract.withdraw(address, amountInWei);
-//     await tx.wait();
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
 export function convertEpochToLocalTime(epoch) {
   // convert epoch seconds to milliseconds
   const date = new Date(epoch * 1000);
 
   // options for date format
   const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
   };
 
   // format the date
-  return date.toLocaleString('en-US', options).replace(', ', ' at ');
+  return date.toLocaleString("en-US", options).replace(", ", " at ");
 }
