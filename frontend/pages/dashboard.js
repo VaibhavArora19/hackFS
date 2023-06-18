6c619946062f8f4cf24e8b0f7aeb56db2aac5336
Aman-Mandal <002amankumar@gmail.com>
2023-06-18 12:03:28 +0530

    Added changes for ens and drand input

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,10 +1,10 @@
-import Job from "@/components/Job/Job";
-import React, { useState, useEffect } from "react";
-import { BsArrowDownShort } from "react-icons/bs";
-import { useAccount } from "wagmi";
-import LIT from "@/components/LIT/LIT";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { BackendUri } from "@/lib/constants";
+import Job from '@/components/Job/Job';
+import React, { useState, useEffect } from 'react';
+import { BsArrowDownShort } from 'react-icons/bs';
+import { useAccount } from 'wagmi';
+import LIT from '@/components/LIT/LIT';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { BackendUri } from '@/lib/constants';
 const Dashboard = () => {
   const [jobData, setJobData] = useState([]);
   const [timeBasedJob, setTimeBasedJob] = useState([]);
@@ -31,7 +31,7 @@ const Dashboard = () => {
 
       const response = await data.json();
 
-      console.log("response", response);
+      console.log('response', response);
       setTimeBasedJob(response.response.timeBasedJobs);
       setCustomJob(response.response.customJobs);
     } catch (err) {
@@ -39,25 +39,25 @@ const Dashboard = () => {
     }
   };
   return (
-    <div className=" min-h-[95vh] bg-[#1C1C1F]">
-      <div className="w-[95%] mx-auto py-10">
-        {!(address && pkpWallet) ? (
-          <div className="flex flex-col items-center gap-2 w-fit mx-auto mt-44 bg-black py-20 px-20 rounded-xl shadow-2xl ">
+    <div className=' min-h-[95vh] bg-[#1C1C1F]'>
+      <div className='w-[95%] mx-auto py-10'>
+        {!address && !pkpWallet ? (
+          <div className='flex flex-col items-center gap-2 w-fit mx-auto mt-44 bg-black py-20 px-20 rounded-xl shadow-2xl '>
             <LIT setPkpWallet={setPkpWallet} />
-            <p className="text-center text-gray-300">or</p>
+            <p className='text-center text-gray-300'>or</p>
             <ConnectButton />
           </div>
         ) : (
           <div>
-            <h2 className="text-xl flex items-center gap-3 font-semibold text-gray-400 font-Poppins ">
+            <h2 className='text-xl flex items-center gap-3 font-semibold text-gray-400 font-Poppins '>
               My Automated Jobs <BsArrowDownShort size={25} />
             </h2>
 
-            <div className="flex flex-col gap-5 mt-10 ">
+            <div className='flex flex-col gap-5 mt-10 '>
               {timeBasedJob.length > 0 &&
                 timeBasedJob.map((job) => (
                   <Job
-                    automationType="timebased"
+                    automationType='timebased'
                     contractAddress={job.contractAddress}
                     functionName={job.functionName}
                     isExecuted={job.isExecuted}
@@ -74,9 +74,9 @@ const Dashboard = () => {
               {customJob.length > 0 &&
                 customJob.map((job) => (
                   <Job
-                    automationType="customLogic"
+                    automationType='customLogic'
                     contractAddress={job.contractAddress}
-                    functionName="Proxy Call"
+                    // functionName="Proxy Call"
                     isExecuted={true}
                     scheduledTime={job.scheduledTime}
                     jobName={job.name}
@@ -86,6 +86,12 @@ const Dashboard = () => {
                     listOfTimesFuncExectued={job.executionTimeline}
                   />
                 ))}
+
+              {!customJob.length && !timeBasedJob.length ? (
+                <p className='text-center py-20 bg-[#161618] text-white font-semibold font-Poppins text-3xl rounded-2xl'>
+                  No jobs available
+                </p>
+              ) : null}
             </div>
           </div>
         )}
